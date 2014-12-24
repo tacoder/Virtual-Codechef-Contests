ee8d3c659e7129f153d52a3ff1733c0a411a1cc9
tacoder <abhinav.singh21093@gmail.com>
2014-12-24 23:50:12 +0530

    Incomplete search functionality implementation to compete.php

--- compete.php
+++ compete.php
@@ -5,6 +5,7 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<title>Virtual codechef contests</title>
 	<link rel="stylesheet" type="text/css" href="index.css" />
+	<link rel="stylesheet" type="text/css" href="compete.css" />
 	<link rel="icon" href="favicon.ico" type="../image/x-icon"/>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="includes/topnav.js"></script>
@@ -16,32 +17,32 @@
 	</div>
 
 
-	<div id="sidenav">
-		<?php include_once("includes/sidenav.php");?>
-	</div>
 	
-	<div  id="maincontent">
-		<p>Select a contest</p>
+	<div  id="compete">
+		<div id="search-wizard"><form><input type="text" name="query" /><select name="catergory"><option value="ALL" selected>ALL</option><option value="Long challenges">Long challenges</option><option value="Lunch time">Lunch time</option></select><input type="submit" value="Go!" /></form></div>
+		</div>
+	<!--<p>Select a contest</p>
+	
 	<form name="login-form" action="refer.php" method="post">
-		<select name = "code">
-			<option value = "BLANK" selected></option>
-			<?php
-				require("includes/connect.php");
-				if(!mysqli_select_db($con, "contests")){
-					echo "Cannot open database!!".mysqli_error($con);
-				}
-				$query = mysqli_query($con,"select code,name from list order by end desc");
-				$result = $query->fetch_all( MYSQLI_ASSOC);
-				foreach( $result as $row ){
-				   $c = $row['code'];
-				   $r = $row['name'];
-				   echo '<option value = "'.$row['code'].'">'.$row['name'].'</option>';
-				} 
-			?>
-		</select>
-	</input>
-	<input type="submit" value="Compete!">
-</form>
+	<select name = "code">
+		<option value = "BLANK" selected></option>
+		<?php
+			require("includes/connect.php");
+			if(!mysqli_select_db($con, "contests")){
+				echo "Cannot open database!!".mysqli_error($con);
+			}
+			$query = mysqli_query($con,"select code,name from list order by end desc");
+			$result = $query->fetch_all( MYSQLI_ASSOC);
+			foreach( $result as $row ){
+			   $c = $row['code'];
+			   $r = $row['name'];
+			   echo '<option value = "'.$row['code'].'">'.$row['name'].'</option>';
+			} 
+		?>
+	</select>
+	<input type="submit" value="Compete!" />
+	</form>-->
+
 	</div>
 </body>
 
