1a07e9ab19565e00ec851ca0e106ac5733e5cb77
tacoder <abhinav.singh21093@gmail.com>
2015-01-05 07:01:34 +0530

    Added rankings for each contest
    function add_contest_rankings in reload.php adds rankings to sql tables.
    sql query logging is present. All queries made by this function will be
    stored in ranks.txt.
    
    Need to make queries reading previous queries from ranks.txt as well.
    
    compete.js generated again (was destroyed in merge conflict)

--- reload.php
+++ reload.php
@@ -34,7 +34,8 @@ if(!mysqli_query($con,'select 1 from list')){
 		code varchar(20) not null,
 		name varchar(200) not null primary key,
 		start bigint not null,
-		end bigint not null);');
+		end bigint not null,
+		type int not null);');
 }
 
 /*
@@ -43,10 +44,12 @@ if(!mysqli_query($con,'select 1 from list')){
  * so we don't have to parse previously parsed pages. This helps quickly 
  * populating sql tables when the website is installed in a new server.
  */
-
 $log = fopen("test.txt","a+");
 // seek to beginning as a+ sets pointer to end of file.
 fseek($log,0);
+
+// log for ranklist
+$rlog = fopen("ranks.txt","a+");
 
 // Read sql queries from test.txt and perform them until we read eof.
 while(!feof($log)){
@@ -60,6 +63,7 @@ while(!feof($log)){
 		echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
 	}
 }
+
 
 /* Set seek to end so we can write any new queries made during the run of this 
  * script
@@ -82,8 +86,8 @@ function secMonths($month,$year){
 
 //Turns $str in format '2014-08-01 15:00:00' to unix timestamp
 function toUnix($str){
-	//Unix timestamp of jan 1st from 1971 to 2020
-	$unixTS = array(31536000,63072000,94694400,126230400,157766400,189302400,
+	//Unix timestamp of jan 1st from 1970 to 2020
+	$unixTS = array(0,31536000,63072000,94694400,126230400,157766400,189302400,
 			220924800,252460800,283996800,315532800,347155200,378691200,
 			410227200,441763200,473385600,504921600,536457600,567993600,
 			599616000,631152000,662688000,694224000,725846400,757382400,
@@ -96,7 +100,7 @@ function toUnix($str){
 	//Explodes string to all possible integers found inside string.
 	preg_match_all('!\d+!', $str, $matches);
 	
-	$startTime =  $unixTS[$matches[0][0]-1970-1];
+	$startTime =  $unixTS[$matches[0][0]-1970];
 	$startTime += secMonths($matches[0][1],$matches[0][0]);
 	$startTime += 86400*($matches[0][2]-1);
 	$startTime += $matches[0][3]*3600;
@@ -108,8 +112,67 @@ function toUnix($str){
 
 //Add rankings for contests.
 function add_contest_rankings($code){
+    global $rlog;
+    global $con;
+    $MAXPAGE = 100000;
+    $contest = json_decode(file_get_contents('http://www.codechef.com/api/rankings/'.$code.'?sortBy=rank&order=asc&page=1&itemsPerPage=100'),true);
+    $sqlq = "update list set type='".$contest['contest_info']['type']."' where code='".$code."'\n";
+    fwrite($rlog, $sqlq);
+    if(!mysqli_query($con,$sqlq)){
+         echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
+    }
 
+    $sqlq="create table ".$contest['contest_info']['contest_code']."_rank (rank int, handle varchar(14) primary key, score float, time bigint(20), penalty int ";
+    
+    foreach ($contest['problems'] as $pcode) {
+        $sqlq .= ", ".$pcode['code']."_score float default 0 ";
+        if($contest['contest_info']['type'] == '1'){
+            $sqlq .= ", ".$pcode['code']."_time bigint(20) default 0 ";
+            $sqlq .= ", ".$pcode['code']."_penalty int default 0 ";
+        }
+    }
+
+    $sqlq .= ");\n";
+    fwrite($rlog, $sqlq);
+    echo $sqlq."<br />";
+
+
+    if(!mysqli_query($con,$sqlq)){
+         echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
+         echo "<br />Dying<br />";
+    //     die();
+    }
+
+    $curPage = 1;
+
+    do{
+        $curPage += 1;
+        foreach ($contest['list'] as $row) {
+            $sqlq_head = "insert into ".$contest['contest_info']['contest_code']."_rank (rank , handle , score , time , penalty ";
+            $sqlq_tail = ") values (".$row['rank'].", '".$row['user_handle']."', ".$row['score'].", '".toUnix("1970-01-01 ".$row['total_time'])."', ".$row['penalty'];
+            
+            foreach ($row['problems_status'] as $pcode => $pvalue) {
+                $sqlq_head .= ", ".$pcode."_score";
+                $sqlq_tail .= ", '".$pvalue['score']."'";
+                if($contest['contest_info']['type'] == '1'){
+                    $sqlq_head .= ", ".$pcode."_time";
+                    $sqlq_head .= ", ".$pcode."_penalty";
+                    $sqlq_tail .= ", '".toUnix("1970-01-01 ".$pvalue['time'])."'";
+                    $sqlq_tail .= ", '".$pvalue['penalty']."'";
+                }
+            }
+            $sqlq = $sqlq_head.$sqlq_tail.");\n";
+            echo $sqlq."<br />";
+            fwrite($rlog, $sqlq);
+            if(!mysqli_query($con,$sqlq)){
+                 echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
+            }
+        }
+        $contest = json_decode(file_get_contents('http://www.codechef.com/api/rankings/'.$code.'?sortBy=rank&order=asc&page='.$curPage.'&itemsPerPage=100'),true);
+
+    }while ($curPage<=$contest['availablePages']);
 }
+
 
 /*
  * Parses problem($pcode) in contest($code) and creates .php inside the contest
@@ -285,13 +348,13 @@ foreach($html->find('div[id="statusdiv"]') as $div){
   }
 }
 
-$tend = time();
 
 $jsList = fopen("includes/compete.js","w+");
 $query = mysqli_query($con,"select code,name,end from list");
 $resultArray = mysqli_fetch_assoc($query);
 // -------Add function from comtest.php
 
+$tend = time();
 echo "Parsing started at:".$tend."<br />";
 echo "Total Time taken =".($tend-$tstart);
 ?>
--- test.php
+++ test.php
@@ -1,35 +1,123 @@
 <?php 
 require('includes/connect.php');
 set_time_limit (0);
+$tstart = time();
+echo "Parsing started at:".$tstart."<br />";
 
-if(!mysqli_select_db($con, "contests")){
-    if(!mysqli_query($con,"create database contests;")){
-        echo "Cannot create database!!".mysqli_error($con);
-        die();
-    } else {
-        mysqli_select_db($con, "contests");
-    }
+//Returns whether $year is leap or not.
+function isLeap($year){
+    return ($year % 4 == 0)&&($year%100!=0||$year%400==0);
+}
+
+//Returns number of seconds in a month. --i guess
+function secMonths($month,$year){
+    $nDay = array(0,31,59,90,120,151,181,212,243,273,304,334);
+    $nDayLeap = array(0,31,60,91,121,152,182,213,244,274,305,335);
+    if(isLeap($year))
+        return $nDayLeap[$month-1]*86400;
+    return $nDay[$month-1]*86400;
+}
+
+//Turns $str in format '2014-08-01 15:00:00' to unix timestamp
+function toUnix($str){
+    //Unix timestamp of jan 1st from 1970 to 2020
+    $unixTS = array(0,31536000,63072000,94694400,126230400,157766400,189302400,
+            220924800,252460800,283996800,315532800,347155200,378691200,
+            410227200,441763200,473385600,504921600,536457600,567993600,
+            599616000,631152000,662688000,694224000,725846400,757382400,
+            788918400,820454400,852076800,883612800,915148800,946684800,
+            978307200,1009843200,1041379200,1072915200,1104537600,1136073600,
+            1167609600,1199145600,1230768000,1262304000,1293840000,1325376000,
+            1356998400,1388534400,1420070400,1451606400,1483228800,1514764800,
+            1546300800);
+
+    //Explodes string to all possible integers found inside string.
+    preg_match_all('!\d+!', $str, $matches);
     
-}
-$code = "DEC14";
-$MAXPAGE = 100000;
-for ($page = 1; $page <= 1/*$MAXPAGE*/; $page++) { 
-    $contest = json_decode(file_get_contents('http://www.codechef.com/api/rankings/'.$code.'?sortBy=rank&order=asc&page='.$page.'&itemsPerPage=100'),true);
-    //Array ( [contest_code] => MARCH09 [type] => 3 [is_team_based] => )
-    echo $contest['contest_info']['contest_code'];
-    echo $contest['contest_info']['type'];
-    if($contest['contest_info']['is_team_based'])
-        echo "yes";
-    echo sizeof($contest['problems']);
+    $startTime =  $unixTS[$matches[0][0]-1970];
+    $startTime += secMonths($matches[0][1],$matches[0][0]);
+    $startTime += 86400*($matches[0][2]-1);
+    $startTime += $matches[0][3]*3600;
+    $startTime += $matches[0][4]*60;
+    $startTime += $matches[0][5];
 
-
-    $MAXPAGE = $contest['availablePages'];
-    echo "new max = ".$MAXPAGE;
+    return $startTime;
 }
 
 
+$con = selectOrCreateDatabase("contests");
+
+$rlog = fopen("ranks.txt","a+");
+function add_contest_rankings($code){
+    global $rlog;
+    global $con;
+    $MAXPAGE = 100000;
+    $contest = json_decode(file_get_contents('http://www.codechef.com/api/rankings/'.$code.'?sortBy=rank&order=asc&page=1&itemsPerPage=100'),true);
+    $sqlq = "update list set type='".$contest['contest_info']['type']."' where code='".$code."'\n";
+    fwrite($rlog, $sqlq);
+    if(!mysqli_query($con,$sqlq)){
+         echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
+    }
+
+    $sqlq="create table ".$contest['contest_info']['contest_code']."_rank (rank int, handle varchar(14) primary key, score float, time bigint(20), penalty int ";
+    
+    foreach ($contest['problems'] as $pcode) {
+        $sqlq .= ", ".$pcode['code']."_score float default 0 ";
+        if($contest['contest_info']['type'] == '1'){
+            $sqlq .= ", ".$pcode['code']."_time bigint(20) default 0 ";
+            $sqlq .= ", ".$pcode['code']."_penalty int default 0 ";
+        }
+    }
+
+    $sqlq .= ");\n";
+    fwrite($rlog, $sqlq);
+    echo $sqlq."<br />";
 
 
+    if(!mysqli_query($con,$sqlq)){
+         echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
+         echo "<br />Dying<br />";
+    //     die();
+    }
+
+    $curPage = 1;
+
+    do{
+        $curPage += 1;
+        foreach ($contest['list'] as $row) {
+            $sqlq_head = "insert into ".$contest['contest_info']['contest_code']."_rank (rank , handle , score , time , penalty ";
+            $sqlq_tail = ") values (".$row['rank'].", '".$row['user_handle']."', ".$row['score'].", '".toUnix("1970-01-01 ".$row['total_time'])."', ".$row['penalty'];
+            
+            foreach ($row['problems_status'] as $pcode => $pvalue) {
+                $sqlq_head .= ", ".$pcode."_score";
+                $sqlq_tail .= ", '".$pvalue['score']."'";
+                if($contest['contest_info']['type'] == '1'){
+                    $sqlq_head .= ", ".$pcode."_time";
+                    $sqlq_head .= ", ".$pcode."_penalty";
+                    $sqlq_tail .= ", '".toUnix("1970-01-01 ".$pvalue['time'])."'";
+                    $sqlq_tail .= ", '".$pvalue['penalty']."'";
+                }
+            }
+            $sqlq = $sqlq_head.$sqlq_tail.");\n";
+            echo $sqlq."<br />";
+            fwrite($rlog, $sqlq);
+            if(!mysqli_query($con,$sqlq)){
+                 echo "Unsuccessful query: ".$sqlq." : ".mysqli_error($con)."<br />";
+            }
+        }
+        $contest = json_decode(file_get_contents('http://www.codechef.com/api/rankings/'.$code.'?sortBy=rank&order=asc&page='.$curPage.'&itemsPerPage=100'),true);
+
+    }while ($curPage<=$contest['availablePages']);
+}
+$query = mysqli_query($con,"select code from list order by end");
+
+while ($row = mysqli_fetch_assoc($query)){ 
+    add_contest_rankings($row["code"]);
+}
+
+$tend = time();
+echo "Parsing ended at:".$tend."<br />";
+echo "Total Time taken =".($tend-$tstart);
 ?>
 
 
